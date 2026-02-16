6eef40e5b6a0086ae127c92cafcc471b518430ea
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-02-16 09:48:04 +0000

    Assg.7

--- ll.h
+++ ll.h
@@ -3,6 +3,8 @@
 struct Node {
    int data; // each listNode contains a character
    struct Node *nextPtr; // pointer to next node
+   struct Node *pPtr;
+   char name[50];
 }; // end structure listNode
 
 typedef struct Node LLnode; // synonym for struct listNode
@@ -12,9 +14,12 @@ typedef LLnode *LLPtr; // synonym for ListNode*
 
 int deletes( LLPtr *sPtr, int value );
 int isEmpty( LLPtr sPtr );
-void insert( LLPtr *sPtr, int value );
-void printList( LLPtr currentPtr );
 void instructions( void );
+void insert( LLPtr *sPtr, int value ,char* name );
+void cleanNode( LLPtr currentPtr);
+void printList( LLPtr currentPtr );
+void printListR( LLPtr currentPtr );
+
 
 
 // display program instructions to user
@@ -27,7 +32,7 @@ void instructions( void )
 } // end function instructions
 
 // insert a new value into the list in sorted order
-void insert( LLPtr *sPtr, int value )
+void insert( LLPtr *sPtr, int value , char* name )
 {
    LLPtr newPtr; // pointer to new node
    LLPtr previousPtr; // pointer to previous node in list
@@ -37,7 +42,9 @@ void insert( LLPtr *sPtr, int value )
 
    if ( newPtr != NULL ) { // is space available
       newPtr->data = value; // place value in node
+      strcpy(newPtr->name,name);
       newPtr->nextPtr = NULL; // node does not link to another node
+      newPtr->pPtr = NULL;
     
        
       previousPtr = NULL;
@@ -51,16 +58,20 @@ void insert( LLPtr *sPtr, int value )
 
       // insert new node at beginning of list
       if ( previousPtr == NULL ) {
+
          newPtr->nextPtr = *sPtr;
+         if ((*sPtr)!=NULL) (*sPtr)->pPtr = newPtr;
       
          *sPtr = newPtr;
         
       } // end if
       else { // insert new node between previousPtr and currentPtr
-         previousPtr->nextPtr = newPtr;
-   
-          
+         previousPtr->nextPtr = newPtr;   
+         newPtr->pPtr = previousPtr;
+
          newPtr->nextPtr = currentPtr;
+         if(currentPtr != NULL) currentPtr->pPtr = newPtr;
+         
  
          
       } // end else
@@ -81,12 +92,15 @@ int deletes( LLPtr *sPtr, int value )
    if ( value == ( *sPtr )->data ) {
       tempPtr = *sPtr; // hold onto node being removed
       *sPtr = ( *sPtr )->nextPtr; // de-thread the node
+      if(*sPtr) (*sPtr)->pPtr = NULL;
       free( tempPtr ); // free the de-threaded node
       return value;
    } // end if
    else {
       previousPtr = *sPtr;
       currentPtr = ( *sPtr )->nextPtr;
+      if(currentPtr) currentPtr->pPtr = *sPtr;
+
 
       // loop to find the correct location in the list
       while ( currentPtr != NULL && currentPtr->data != value ) {
@@ -98,6 +112,7 @@ int deletes( LLPtr *sPtr, int value )
       if ( currentPtr != NULL ) {
          tempPtr = currentPtr;
          previousPtr->nextPtr = currentPtr->nextPtr;
+         if(currentPtr->nextPtr) currentPtr->nextPtr->pPtr = previousPtr;
          free( tempPtr );
          return value;
       } // end if
@@ -124,14 +139,31 @@ void printList( LLPtr currentPtr )
 
       // while not the end of the list
       while ( currentPtr->nextPtr!= NULL ) {
-         printf( "%d --> ", currentPtr->data );
+         printf( "%d %s --> ", currentPtr->data, currentPtr->name );
          currentPtr = currentPtr->nextPtr;
       } // end while
+       printf( "%d %s -->NULL\n",currentPtr->data ,currentPtr->name);
 
-      printf( "%d --> NULL\n",currentPtr->data );
-       
+      while(currentPtr->nextPtr != NULL){
+         currentPtr = currentPtr->nextPtr;
+      }
 
+      while(currentPtr->pPtr != NULL){
+         printf("%d %s --> ",currentPtr->data ,currentPtr->name);
+         currentPtr = currentPtr->pPtr; 
+      }
+      printf( "%d %s -->NULL\n",currentPtr->data ,currentPtr->name);
+   }
+}
+
+void cleanNode( LLPtr currentPtr )
+{
+      while(!isEmpty(currentPtr)){
+      printf("deletes %d \n",currentPtr->data);
+      deletes( &currentPtr, currentPtr->data);
+      }
+}
      
        
-   } // end else
-} // end function printList
+ // end else
+ // end function printList
