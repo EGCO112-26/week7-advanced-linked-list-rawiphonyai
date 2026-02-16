cc73fe687081a21db9b8a86b6cc99982ac3fe802
rawiphonyai <rawiphon.yai@student.mahidol.edu>
2026-02-16 09:50:18 +0000

    Assg.7

--- ll.h
+++ ll.h
@@ -135,11 +135,11 @@ void printList( LLPtr currentPtr )
       puts( "List is empty.\n" );
    } // end if
    else {
-      puts( "The list is:" );
+   // puts( "The list is:" );
 
       // while not the end of the list
       while ( currentPtr->nextPtr!= NULL ) {
-         printf( "%d %s --> ", currentPtr->data, currentPtr->name );
+         printf( "%d %s -->", currentPtr->data, currentPtr->name );
          currentPtr = currentPtr->nextPtr;
       } // end while
        printf( "%d %s -->NULL\n",currentPtr->data ,currentPtr->name);
@@ -149,7 +149,7 @@ void printList( LLPtr currentPtr )
       }
 
       while(currentPtr->pPtr != NULL){
-         printf("%d %s --> ",currentPtr->data ,currentPtr->name);
+         printf("%d %s -->",currentPtr->data ,currentPtr->name);
          currentPtr = currentPtr->pPtr; 
       }
       printf( "%d %s -->NULL\n",currentPtr->data ,currentPtr->name);
